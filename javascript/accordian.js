560661d523320997db0930cebe5c0212bc990453
Steve Han <stevehan2001@gmail.com>
2018-08-11 14:42:35 -0500

    Book gallery

---
+++ javascript/accordian.js
@@ -0,0 +1,21 @@
+var books = document.getElementsByClassName("book-covers");
+var details = document.getElementsByClassName("details");
+for (let i = 0; i < books.length; i++) {
+    books[i].addEventListener("click", () => {
+        toggleShow(books[i], details[i]);
+        for(let j = 0; j < books.length; j ++) {
+            if(books[j].classList.contains("selected") && j!==i) {
+                toggleShow(books[j], details[j]);
+            }
+        }
+    })
+}
+
+function toggleShow(book, detail){
+    if (detail.style.maxHeight) {
+        detail.style.maxHeight = null;
+    } else {
+        detail.style.maxHeight = detail.scrollHeight + "px";
+    }
+    book.classList.toggle("selected");
+}
