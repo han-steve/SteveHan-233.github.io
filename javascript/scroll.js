7060587e605aae5a8dbc6eede628f8b0ca7690a5
Steve Han <stevehan2001@gmail.com>
2018-08-19 10:08:50 -0500

    bug fixes and scroll animation

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,11 +1,13 @@
 var elements = document.getElementsByClassName("scroll-right");
 for(var i = 0; i < elements.length; i ++) {
     var element = elements[i];
+    element.style.visibility = "hidden";
     $(window).on('scroll' , function(){
         var scroll_pos = $(window).scrollTop() + $(window).height();
         var element_pos = $(element).offset().top;
         if (scroll_pos > element_pos) {
             $(element).addClass('animated fadeInRightBig');
+            element.style.visibility = "visible";
         };
     
     })
